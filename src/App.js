5ba0827581490934b4f10d49baf8cb59f97e9e19
Rob Horner <rdhjr81@gmail.com>
2022-11-20 10:00:20 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Route, Switch } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+
+function App() {
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Switch>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/products/:productId">
+          <ProductDetail />
+        </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
